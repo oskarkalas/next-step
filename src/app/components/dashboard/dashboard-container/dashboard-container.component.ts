ac91e2577629c1f803a24762ddf16727b71e8d1e
Oskar Kalaš <o.kalas@asiana.cz>
2024-08-15 22:09:55 +0200

    update formly and structure

---
+++ src/app/components/dashboard/dashboard-container/dashboard-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-container.component.html',
+  styles: ``
+})
+export class DashboardContainerComponent {
+
+}
